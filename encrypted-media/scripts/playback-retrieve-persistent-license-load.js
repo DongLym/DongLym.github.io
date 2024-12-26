8d44a54411c857fb9a83f710e96ef7fc46dbd031
DongLym <49906824+DongLym@users.noreply.github.com>
2024-12-26 10:02:00 +0800

    Update playback-retrieve-persistent-license-load.js

--- encrypted-media/scripts/playback-retrieve-persistent-license-load.js
+++ encrypted-media/scripts/playback-retrieve-persistent-license-load.js
@@ -108,7 +108,7 @@ function runTest(config,qualifier) {
             return _video.setMediaKeys( mediaKeys );
         }).then(function() {
             _mediaKeySession = _mediaKeys.createSession('persistent-license');
-            _sessionId = "38599F4001000000";
+            _sessionId = "A172B46901000000";
             return _mediaKeySession.load(_sessionId);
         }).then(function( success ) {
             console.log("[NEU][LOAD]load id: " + event.data.sessionId + ", success: " + success);
